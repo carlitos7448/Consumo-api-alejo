f57aba0cb7afe27ecaf9fb85f4876c0714c63cc5
carlos <carlosrive367@gmail.com>
2026-06-08 14:46:36 -0500

    feat: implement user service integration

--- src/app/components/user-list/user-list.ts
+++ src/app/components/user-list/user-list.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserService } from '../../services/user';
 
 @Component({
   selector: 'app-user-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './user-list.html',
   styleUrl: './user-list.css',
 })
-export class UserList {}
+export class UserList {
+
+  usuarios:any[] = [];
+
+  constructor(private userService: UserService){
+
+  }
+
+  ngOnInit(){
+
+    this.userService.obtenerUsuarios().subscribe((data:any) => {
+
+      this.usuarios = data;
+
+    });
+
+  }
+
+}
